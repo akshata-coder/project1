adb1e2c08589b2b9066153bde7e2bb02dd0f2d0b
akshata-coder <akshata@gotechagency.com>
2019-07-17 16:36:10 +0530

    akshata updated the file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel sixth
+                    Laravel first
                 </div>
 
                 <div class="links">
