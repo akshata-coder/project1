713ad9decd840d3a5412ada661edc47682290f72
sudhacoder <52994781+sudhacoder@users.noreply.github.com>
2019-07-17 13:05:32 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel FIRST
                 </div>
 
                 <div class="links">
