991711c234a34d06df910add7ce61be5216609e0
akshata-coder <52992026+akshata-coder@users.noreply.github.com>
2019-07-17 13:16:13 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Second
+                    Laravel Third
                 </div>
 
                 <div class="links">
